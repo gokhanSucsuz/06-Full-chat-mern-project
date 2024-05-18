21a0f88f3ed09facadad870e6ca6030ead6bb6b8
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-18 13:08:27 +0300

    chat page design is created

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react'
 import { UserContext } from './UserContext'
 import RegisterAndLoginForm from './RegisterAndLoginForm'
+import { Chat } from './Chat'
 
 export const Routes = () => {
     const { username } = useContext(UserContext)
 
-    if (username) { return "Logged in " + username }
+    if (username) { return <Chat /> }
     return (
         <RegisterAndLoginForm />
     )
