cb79279604aabe9beaea35f1193b74f118cbf7b7
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-25 16:26:18 +0300

    ...

--- api/index.js
+++ api/index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(cors());
+app.options("*", cors());
 
 const jwtSecret = process.env.JWT_SECRET;
 
