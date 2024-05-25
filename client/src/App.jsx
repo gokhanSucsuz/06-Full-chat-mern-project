96248e47e1cb91b1b06e96d2d9cdfd6402b46fa5
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-25 15:42:34 +0300

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Routes } from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://06-full-chat-mern-project-api-server.vercel.app';
+  axios.defaults.baseURL = 'https://06-full-chat-mern-project-api-server.vercel.app/';
   axios.defaults.withCredentials = true;
   return (
     <Routes />
