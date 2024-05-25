444818da6e10e0f7ffcfbf2fb959a6f670ff7756
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-25 14:03:55 +0300

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Routes } from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://06-full-chat-mern-project-deru49hnr-coolpisces-projects.vercel.app';
+  axios.defaults.baseURL = 'https://06-full-chat-mern-project.vercel.app/';
   axios.defaults.withCredentials = true;
   return (
     <Routes />
