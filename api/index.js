9df81db0871e8922900d44230cd26a0c34e6765d
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-25 16:30:03 +0300

    ....

--- api/index.js
+++ api/index.js
@@ -22,7 +22,12 @@ mongoose.connect(process.env.MONGO_URL);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://06-full-chat-mern-project-wtln.vercel.app",
+		credentials: true,
+	})
+);
 app.options("*", cors());
 
 const jwtSecret = process.env.JWT_SECRET;
